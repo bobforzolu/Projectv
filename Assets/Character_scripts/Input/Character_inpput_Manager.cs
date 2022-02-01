aaeedb915fcafe822fdb3916527672f074fca0ab
Frohjur <69992260+bobforzolu@users.noreply.github.com>
2022-02-01 01:20:53 -0700

    testing starts

---
+++ Assets/Character_scripts/Input/Character_inpput_Manager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class Character_inpput_Manager : MonoBehaviour
+{
+    private Vector2 playerMovement;
+     public void OnMoveInput(InputAction.CallbackContext context){
+
+    }
+     public void OnJumpInput(InputAction.CallbackContext context){
+
+    }
+}
--- Assets/Character_scripts/StateMachine/PlayerState.cs
+++ Assets/Character_scripts/StateMachine/PlayerState.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player_Input_manager : MonoBehaviour
+public class PlayerState : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
